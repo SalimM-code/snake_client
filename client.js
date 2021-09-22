045d15ab3f47707694e08558f40abf6f25bdb6d1
salimM-code <mustafasalim000@gmail.com>
2021-09-22 19:38:36 -0400

    modified connect func

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 
 const { ClientRequest } = require("http");
 const net = require("net");
+const { IP, PORT } = require('./constant');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542,
+    host: IP,
+    port: PORT
     
   });
 
