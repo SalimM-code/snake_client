932b14992c1877316757b570efac0b7abe7352e3
salimM-code <mustafasalim000@gmail.com>
2021-09-22 18:35:41 -0400

    added more logic

--- play.js
+++ play.js
@@ -1,7 +1,39 @@
 
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', movementInput)
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+
+  if(key === '\u0003') {
+    process.exit();
+  }
+}
+const movementInput = (key) => {
+  if(key === 'w') {
+    conn.write('Move: up');
+  }
+  if(key === 's') {
+    conn.write('Move: down');
+  }
+  if(key === 'a') {
+
+    conn.write('Move: left');
+  }
+  if((key === 'd')) {
+    conn.write('Move: right');
+  }
+}
 
 console.log("Connecting ...");
 connect();
+setupInput();
 
